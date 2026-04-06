6a5893eeae2f0a97633799cc8600ce21ffd60726
Tati Carvalho <tati130113@gmail.com>
2026-04-06 14:22:27 -0300

    feat: listagem de produtos funcionando

--- src/main/java/pizzaria/view/TelaProdutos.java
+++ src/main/java/pizzaria/view/TelaProdutos.java
@@ -1,5 +1,6 @@
 package pizzaria.view;
 // commit 1 - botão salvar funcionando
+// commit 2 - listagem de produtos funcionando
 import pizzaria.dao.ProdutoDAO;
 import pizzaria.dao.CategoriaDAO;
 import pizzaria.model.Produto;
